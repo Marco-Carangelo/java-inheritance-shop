52824ea4eb70d3cb23d786a5f137c1d4feb02e85
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-20 16:27:34 +0200

    add: new Smartphone istance created and tested

--- src/org/lessons/inheritanceshop/Main.java
+++ src/org/lessons/inheritanceshop/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Smartphone newCell = new Smartphone("Edge 40" , "Motorola" , 250 , 22 , "1234567890123456" , 128);
+		
+		System.out.println("Caratteristiche smartphone: ");
+		System.out.println("Nome: " + newCell.getProdName());
+		System.out.println("Marca: " + newCell.getProdBrand());
+		System.out.printf("Prezzo base: %.2f €\n", newCell.getProdPrice());
+		System.out.println("Iva applicata: " + newCell.getProdIva());
+		System.out.println("Codice IMEI: " + newCell.getImeiCod());
+		System.out.println("Capacità di memoria: " + newCell.getMemory());
+		System.out.printf("Prezzo + IVA: %.2f €\n", newCell.getTaxedPrice());
+		System.out.println("Codice numerico: " + newCell.getProdCode());
+		System.out.println("Codice completo: " + newCell.getFullProdName());
+		
+		
+		
 		
 
 	}

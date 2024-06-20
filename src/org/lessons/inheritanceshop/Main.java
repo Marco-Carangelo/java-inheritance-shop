0f6aa26e338923d46b25a4cbd1874f33c78248f7
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-20 16:55:19 +0200

    add: Televisore class attributes and methods tested

--- src/org/lessons/inheritanceshop/Main.java
+++ src/org/lessons/inheritanceshop/Main.java
@@ -20,7 +20,31 @@ public class Main {
 		System.out.println("Codice completo: " + newCell.getFullProdName());
 		
 		
+		//Creazione di un'istannza della classe Smartphone e stampa di prova di metodi e attributi
 		
+		Televisore newTv = new Televisore("BohTV" , "Samsung" , 500 , 22 ,(byte) 52 , true);
+		
+		//Dichiariamo una variabile che useremo per visualizzare se il televisore è smart oppure no in forma più leggibile
+		String isSmart;
+		
+		//Verifichiamo il valore della variabile telSmart, se vero daremo ad isSmart valroe "Sì", altrimenti valore "No"
+		if (newTv.isTelSmart()) {
+			isSmart = "Sì";
+		}else {
+			isSmart = "No";
+		}
+			
+				
+		System.out.println("\n\n\nCaratteristiche Televisore: ");
+		System.out.println("Nome: " + newTv.getProdName());
+		System.out.println("Marca: " + newTv.getProdBrand());
+		System.out.printf("Prezzo base: %.2f €\n", newTv.getProdPrice());
+		System.out.println("Iva applicata: " + newTv.getProdIva());
+		System.out.println("Dimensione in pollici: " + newTv.getTelSize() + "\"");
+		System.out.println("SmartTV: " + isSmart);
+		System.out.printf("Prezzo + IVA: %.2f €\n", newTv.getTaxedPrice());
+		System.out.println("Codice numerico: " + newTv.getProdCode());
+		System.out.println("Codice completo: " + newTv.getFullProdName());
 		
 
 	}

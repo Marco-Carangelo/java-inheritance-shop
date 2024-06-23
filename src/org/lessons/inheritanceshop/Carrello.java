b996a5a3288ede6eb27f1f89a15e10db3feacde8
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-24 00:08:32 +0200

    add: program variables and arrays

--- src/org/lessons/inheritanceshop/Carrello.java
+++ src/org/lessons/inheritanceshop/Carrello.java
@@ -1,9 +1,33 @@
 package org.lessons.inheritanceshop;
 
+import java.util.Scanner;
+
 public class Carrello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Creiamo un'istanza della classe scanner
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//Creiamo degli arrai vuoti che possano contenere i vari prodotti inseriti
+		
+		Smartphone smartphones[] = new Smartphone[10];
+		Televisore televisori[] = new Televisore[10];
+		Cuffie cuffie[] = new Cuffie[10];
+		
+		//Variabile di controllo per l'uscita dal ciclo while
+		boolean scelta = true;
+		
+		//Variabile per la scelta del prodotto da inserire
+		byte sceltaProdotto = 0;
+		
+		
+		
+		
+		
+		
+		
 
 	}
 

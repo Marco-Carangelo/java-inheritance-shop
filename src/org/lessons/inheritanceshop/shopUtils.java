23d1f78a86793285fc2ad8a2023a20e59770ced3
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-24 14:52:18 +0200

    add: method to manage the choice between true and false

--- src/org/lessons/inheritanceshop/shopUtils.java
+++ src/org/lessons/inheritanceshop/shopUtils.java
@@ -1,13 +1,49 @@
 package org.lessons.inheritanceshop;
 
+import java.util.Scanner;
+
 public class shopUtils {
 	
 	//Metodo per generare un numero random tra un valore minimo ed un massimo
 	
-		static public int randomCodeGenerator( int min, int max) {
+	static public int randomCodeGenerator( int min, int max) {
+				
+		return (int) (Math.random() * (max - min + 1)) + min;
+	}
+	
+	
+	//Metodo per gestire una tra due valore inserendo S per Sì ed N per No
+	
+	static public boolean choiceManager() {
+		
+		boolean choice = false;
+		char userChoice = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(userChoice != 's' && userChoice != 'n') {
+			
+			System.out.println("Premere S per Sì o N per No");
+			
+			userChoice = scanner.next().toLowerCase().charAt(0);	
+			
+				if(userChoice == 's') {
 					
-			return (int) (Math.random() * (max - min + 1)) + min;
-		}
+					choice = true;
+					
+				}else if(userChoice == 'n') {
+					
+					choice = false;
+					
+					}else {
+						
+						System.out.println("Il valore inserito non è valido");
+						
+					}
+			}
+		return choice;
+	}
+	
 		
 
 }
